817c81fc22aff25c7c5bc5a96547b2c2c6727436
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-05-19 12:46:13 +0330

    Add models Add all required interfaces for user module

--- internal/domains/user/models.go
+++ internal/domains/user/models.go
@@ -1,1 +1,42 @@
 package user
+
+import "time"
+
+// User struct defines user entity which is used in database
+type User struct {
+	Id          int        `json:"-" gorm:"primaryKey"`
+	Uuid        string     `json:"uuid" gorm:"uniqueIndex"`
+	PhoneNumber string     `json:"phone_number" gorm:"uniqueIndex"`
+	Name        *string    `json:"name,omitempty"`
+	Password    string     `json:"password"`
+	LastLoginAt *time.Time `json:"last_login_at,omitempty"`
+	CreatedAt   time.Time  `json:"created_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+}
+
+// UserRegisterRequest defines a struct for user creation operation
+type UserRegisterRequest struct {
+	PhoneNumber     string  `json:"phone_number" validate:"required,len=11"`
+	Password        string  `json:"password" validate:"required,min:8,max:255"`
+	PasswordConfirm string  `json:"password_confirm" validate:"required,min=8,max=255,eqfield=Password"`
+	Name            *string `json:"name,omitempty" validate:"omitempty,min=3,max=255"`
+}
+
+// UserLoginRequest defines a fields for login operation and appropriate validation rules for that too
+type UserLoginRequest struct {
+	PhoneNumber string `json:"phone_number" validate:"required,len=11"`
+	Password    string `json:"password" validate:"required,min:8,max:255"`
+}
+
+// UpdateUserRequest defines a struct which contains necessary fields and data for updating user operation
+type UpdateUserRequest struct {
+	Name            *string `json:"name,omitempty" validate:"omitempty,min:3,max=255"`
+	Password        *string `json:"password,omitempty" validate:"omitempty,min:8,max:255"`
+	PasswordConfirm *string `json:"password_confirm,omitempty" validate:"omitempty,min=8,max=255,eqfield=Password"`
+}
+
+// AuthResponse defines struct for authentication operation that contains necessary fields for auth operation
+type AuthResponse struct {
+	Tokens map[string]string `json:"authorization"`
+	User   *User             `json:"user"`
+}
--- internal/domains/user/user.go
+++ internal/domains/user/user.go
@@ -1,1 +1,30 @@
 package user
+
+import "context"
+
+// UserRepositoryInterface defines set of methods which every repository who wants to play role as user repo should obey.
+type UserRepositoryInterface interface {
+	GetUserById(id int) (*User, error)
+	GetUserByUUID(uuid string) (*User, error)
+	GetUserByPhone(phone string) (*User, error)
+	UserExists(phone string) bool
+	CreateUser(user *User) (*User, error)
+	UpdateUser(user *User, name, password *string) (*User, error)
+}
+
+// UserServiceInterface defines set of methods which every service who wants to play role as user service should obey.
+type UserServiceInterface interface {
+	GetUserById(id int) (*User, error)
+	GetUserByUUID(uuid string) (*User, error)
+	GetUserByPhone(phone string) (*User, error)
+	CreateUser(name *string, phone, password string) (*User, error)
+	UpdateUser(userId int, name, password *string) (*User, error)
+}
+
+// UserUseCaseInterface defines set of methods which every use case who wants to play role as user use case should obey.
+type UserUseCaseInterface interface {
+	Register(ctx context.Context, request *UserRegisterRequest) (*AuthResponse, error)
+	Login(ctx context.Context, request *UserLoginRequest) (*AuthResponse, error)
+	UpdateUserPass(ctx context.Context, request *UpdateUserRequest) (*User, error)
+	UpdateUserName(ctx context.Context, request *UpdateUserRequest) (*User, error)
+}
