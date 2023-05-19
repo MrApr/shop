96201bcbd65baf58ffd8bcc7405b78ec9fe9903e
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-05-19 18:48:23 +0330

    Add user service with it's tests

--- internal/domains/user/service.go
+++ internal/domains/user/service.go
@@ -1,1 +1,77 @@
 package user
+
+import (
+	"shop/pkg/advancedError"
+	"shop/pkg/hasher"
+	"shop/pkg/uuid"
+)
+
+// UserService which implements service interface functionalities and satisfies it
+type UserService struct {
+	repo UserRepositoryInterface
+}
+
+// NewService instantiates and returns new user service
+func NewService(repo UserRepositoryInterface) UserServiceInterface {
+	return &UserService{
+		repo: repo,
+	}
+}
+
+// GetUserById and return it
+func (u *UserService) GetUserById(id int) (*User, error) {
+	return u.repo.GetUserById(id)
+}
+
+// GetUserByUUID and return it
+func (u *UserService) GetUserByUUID(uuid string) (*User, error) {
+	return u.repo.GetUserByUUID(uuid)
+}
+
+// GetUserByPhone and return it
+func (u *UserService) GetUserByPhone(phone string) (*User, error) {
+	return u.repo.GetUserByPhone(phone)
+}
+
+// CreateUser that doesn't exist in repository
+func (u *UserService) CreateUser(name *string, phone, password string) (*User, error) {
+	isExists := u.repo.UserExists(phone)
+	if isExists {
+		return nil, UserAlreadyExists
+	}
+
+	hashedPass, err := hasher.Make(password)
+	if err != nil {
+		return nil, advancedError.New(err, "user hashing password failed")
+	}
+
+	generatedUUID, err := uuid.GenerateUUId()
+	if err != nil {
+		return nil, advancedError.New(err, "user generate uuid failed")
+	}
+
+	user := &User{
+		Id:          0,
+		UUID:        generatedUUID,
+		PhoneNumber: phone,
+		Name:        name,
+		Password:    hashedPass,
+	}
+
+	return u.repo.CreateUser(user)
+}
+
+// UpdateUser that already exists in database
+func (u *UserService) UpdateUser(userId int, name, password *string) (*User, error) {
+	user, err := u.repo.GetUserById(userId)
+	if err != nil {
+		return nil, UserDoesntExists
+	}
+
+	if password != nil {
+		hashedPass, _ := hasher.Make(*password)
+		password = &hashedPass
+	}
+
+	return u.repo.UpdateUser(user, name, password)
+}
---
+++ internal/domains/user/service_test.go
@@ -0,0 +1,101 @@
+package user
+
+import (
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+	"math/rand"
+	"testing"
+)
+
+// TestUserService_GetUserById functionality
+func TestUserService_GetUserById(t *testing.T) {
+	db, err := setupDbConnection()
+	assert.NoError(t, err, "Setup database connection failed")
+
+	service := createService(db)
+	users := mockAndInsertUser(db, 1)
+
+	fetchedUser, err := service.GetUserById(users[0].Id)
+	assertUsersEquality(t, fetchedUser, &users[0])
+
+	randId := rand.Int()
+	_, err = service.GetUserById(randId)
+	assert.Error(t, err, "Fetching wrong user from db failed ! it should throw an error")
+}
+
+// TestUserService_GetUserByUUID functionality
+func TestUserService_GetUserByUUID(t *testing.T) {
+	db, err := setupDbConnection()
+	assert.NoError(t, err, "Setup database connection failed")
+
+	service := createService(db)
+	users := mockAndInsertUser(db, 1)
+
+	fetchedUser, err := service.GetUserByUUID(users[0].UUID)
+	assertUsersEquality(t, fetchedUser, &users[0])
+
+	randUUID := "Test2UUid"
+	_, err = service.GetUserByUUID(randUUID)
+	assert.Error(t, err, "Fetching wrong user from db failed ! it should throw an error")
+}
+
+// TestUserService_GetUserByPhone functionality
+func TestUserService_GetUserByPhone(t *testing.T) {
+	db, err := setupDbConnection()
+	assert.NoError(t, err, "Setup database connection failed")
+
+	service := createService(db)
+	users := mockAndInsertUser(db, 1)
+
+	fetchedUser, err := service.GetUserByPhone(users[0].PhoneNumber)
+	assertUsersEquality(t, fetchedUser, &users[0])
+
+	randPhone := "09191234567"
+	_, err = service.GetUserByPhone(randPhone)
+	assert.Error(t, err, "Fetching wrong user from db failed ! it should throw an error")
+}
+
+// TestUserService_CreateUser functionality
+func TestUserService_CreateUser(t *testing.T) {
+	db, err := setupDbConnection()
+	assert.NoError(t, err, "Setup database connection failed")
+
+	service := createService(db)
+	mockedUser := mockUser()
+
+	createdUser, err := service.CreateUser(mockedUser.Name, mockedUser.PhoneNumber, mockedUser.Password)
+	assert.NoError(t, err, "User service user creation failed")
+	assert.NotEqual(t, createdUser.Password, mockedUser.Password, "User service user creation failed")
+	assert.NotZero(t, createdUser.Id, "User service user creation failed")
+
+	_, err = service.CreateUser(mockedUser.Name, mockedUser.PhoneNumber, mockedUser.Password)
+	assert.Error(t, err, "User service user creation failed")
+	assert.ErrorIs(t, err, UserAlreadyExists, "User service user creation failed")
+}
+
+// TestUserService_UpdateUser functionality
+func TestUserService_UpdateUser(t *testing.T) {
+	db, err := setupDbConnection()
+	assert.NoError(t, err, "Setup database connection failed")
+
+	service := createService(db)
+	users := mockAndInsertUser(db, 1)
+
+	newName := "newlyyyname"
+	pass := "Password"
+
+	updatedUser, err := service.UpdateUser(users[0].Id, &newName, &pass)
+	assert.NoError(t, err, "User service update user failed")
+	assert.Equal(t, *updatedUser.Name, newName, "User service update user failed")
+	assert.NotEqual(t, pass, updatedUser.Password, "User service update user failed")
+
+	randId := rand.Int()
+	_, err = service.UpdateUser(randId, &newName, &pass)
+	assert.Error(t, err, "User service update user failed")
+	assert.ErrorIs(t, err, UserDoesntExists, "User service update user failed")
+}
+
+// createService and return it for testing purpose
+func createService(db *gorm.DB) UserServiceInterface {
+	return NewService(NewRepository(db))
+}
