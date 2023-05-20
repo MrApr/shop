e50066a281009acfeee30c0d1e9d7a505209bc4b
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-05-20 20:49:51 +0330

    Add required models Add all abstractions and interface in order to get implemented

--- internal/domains/address/address.go
+++ internal/domains/address/address.go
@@ -1,1 +1,33 @@
 package address
+
+import "context"
+
+// AddressRepositoryInterface defines set of methods that each type who wants to play role as address repository should obey
+type AddressRepositoryInterface interface {
+	GetAllCities() ([]City, error)
+	GetAddressById(id int) (*Address, error)
+	GetAllUserAddresses(userId int) ([]Address, error)
+	CreateAddress(address *Address) (*Address, error)
+	UpdateAddress(address *Address, cityId int, newAddress string) (*Address, error)
+	DeleteAddress(address *Address) (bool, error)
+}
+
+// AddressServiceInterface defines set of methods that each type who wants to play role as address service should obey
+type AddressServiceInterface interface {
+	GetAllCities() ([]City, error)
+	GetAllUserAddresses(userId int) ([]Address, error)
+	CreateAddress(userId, cityId int, address string) (*Address, error)
+	UpdateAddress(requestedId, addressId, cityId int, newAddress string) (*Address, error) //Todo add Authorization on address edit and delete operation check user is the owner or not
+	DeleteAddress(requestedId, addressId int) error
+}
+
+// AddressUseCaseInterface defines set of methods that each type who wants to play role as address use-case should obey
+type AddressUseCaseInterface interface {
+	GetAllCities(ctx context.Context) ([]City, error)
+	GetAllUserAddresses(ctx context.Context, userId int) ([]Address, error)
+	CreateAddress(ctx context.Context, token string, request *CreateAddressRequest) (*Address, error)
+	UpdateAddress(ctx context.Context, token string, request *UpdateAddressRequest) (*Address, error)
+	DeleteAddress(ctx context.Context, token string, request *DeleteAddressRequest) error
+}
+
+//Todo add global package for handing Authorization in all domains
--- internal/domains/address/models.go
+++ internal/domains/address/models.go
@@ -1,1 +1,39 @@
 package address
+
+import "time"
+
+// Address is a struct which defines address entity fields and properties in system
+type Address struct {
+	Id        int        `json:"id" gorm:"primaryKey"`
+	UserId    int        `json:"-"`
+	CityId    int        `json:"city_id"`
+	City      City       `json:"city" gorm:"foreignKey:CityId;references:Id"`
+	Address   string     `json:"address"`
+	CreatedAt *time.Time `json:"created_at"`
+	UpdateAt  *time.Time `json:"update_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
+}
+
+// City is a struct which contains city's entity fields
+type City struct {
+	Id    int    `json:"id" gorm:"primaryKey"`
+	Title string `json:"title"`
+}
+
+// CreateAddressRequest defines set of parameters for user address creation
+type CreateAddressRequest struct {
+	CityId  int    `json:"city_id" validate:"required,numeric,min=1"`
+	Address string `json:"address" validate:"required,min=5,max=500"`
+}
+
+// UpdateAddressRequest defines set of parameters for user address modification
+type UpdateAddressRequest struct {
+	AddressId int    `json:"address_id" validate:"required,numeric,min=1"`
+	CityId    int    `json:"city_id" validate:"required,numeric,min=1"`
+	Address   string `json:"address" validate:"required,min=5,max=500"`
+}
+
+// DeleteAddressRequest defines set of methods for user address deletation
+type DeleteAddressRequest struct {
+	AddressId int `json:"address_id" validate:"required,numeric,min=1"`
+}
