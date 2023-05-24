3088ddc31f52bf9b5f490a4398d9af59e6421737
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-05-24 19:59:07 +0330

    Fix categories model Add Categories model's struct Add categories abstractions and contracts

--- internal/domains/products/models.go
+++ internal/domains/products/models.go
@@ -10,3 +10,17 @@ type Type struct {
 	UpdatedAt *time.Time `json:"updated_at,omitempty"`
 	DeletedAt *time.Time `json:"deleted_at,omitempty"`
 }
+
+// Category defines category entity in system
+type Category struct {
+	Id             int        `json:"id" gorm:"primaryKey"`
+	TypeId         int        `json:"type_id"`
+	Type           Type       `json:"type" gorm:"foreignKey:TypeId;references:Id"`
+	ParentCatId    *int       `json:"parent_cat_id,omitempty"`
+	ParentCategory *Category  `json:"parent_category,omitempty" gorm:"foreignKey:ParentCatId;reference:Id"`
+	Title          string     `json:"title"`
+	Indent         int        `json:"indent"`
+	Order          int        `json:"order"`
+	CreatedAt      *time.Time `json:"created_at,omitempty"`
+	UpdatedAt      *time.Time `json:"updated_at,omitempty"`
+}
--- internal/domains/products/products.go
+++ internal/domains/products/products.go
@@ -1,1 +1,18 @@
 package products
+
+import "context"
+
+// CategoriesRepositoryInterface defines set of abstract methods for every type, who is going to be Category Repository
+type CategoriesRepositoryInterface interface {
+	GetAllCategories() []Category
+}
+
+// CategoryServiceInterface defines set of abstract methods for every type, who is going to be Category Service
+type CategoryServiceInterface interface {
+	GetAllCategories() ([]Category, error)
+}
+
+// CategoryUseCaseInterface defines set of abstract methods for every type, who is going to be Category Use-Case
+type CategoryUseCaseInterface interface {
+	GetAllCategories(ctx context.Context) ([]Category, error)
+}
