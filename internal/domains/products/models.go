7ddd88ec076bfd37637f31518f5cd67401ae323d
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-05-24 19:49:45 +0330

    Add types table's model

--- internal/domains/products/models.go
+++ internal/domains/products/models.go
@@ -1,1 +1,12 @@
 package products
+
+import "time"
+
+// Type struct defines type entity
+type Type struct {
+	Id        int        `json:"id" gorm:"primaryKey"`
+	Title     string     `json:"title"`
+	CreatedAt *time.Time `json:"created_at,omitempty"`
+	UpdatedAt *time.Time `json:"updated_at,omitempty"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+}
