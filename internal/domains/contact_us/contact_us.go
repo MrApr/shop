254d1b59658c3b6bf2cf572c1d84154cf959d326
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-05-30 19:43:44 +0330

    Add models and abstractions

--- internal/domains/contact_us/contact_us.go
+++ internal/domains/contact_us/contact_us.go
@@ -1,1 +1,18 @@
 package contact_us
+
+import "context"
+
+// ContactUsRepositoryInterface defines set of method for every type who is contact us repository
+type ContactUsRepositoryInterface interface {
+	CreateContactUs(contactUs *ContactUs) error
+}
+
+// ContactUsServiceInterface defines set of method for every type who is contact us service
+type ContactUsServiceInterface interface {
+	CreateContactUs(title, email, description string) (*ContactUs, error)
+}
+
+// ContactUsUseCaseRepository defines set of method for every type who is contact us use-case
+type ContactUsUseCaseRepository interface {
+	CreateContactUs(ctx context.Context, request *CreateContactUsRequest) (*ContactUs, error)
+}
--- internal/domains/contact_us/models.go
+++ internal/domains/contact_us/models.go
@@ -1,1 +1,20 @@
 package contact_us
+
+import "time"
+
+// ContactUs struct represents contact us entity in system
+type ContactUs struct {
+	Id          int        `json:"id" gorm:"primaryKey"`
+	Email       string     `json:"email"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	CreatedAt   *time.Time `json:"created_at,omitempty"`
+	UpdatedAt   *time.Time `json:"updated_at,omitempty"`
+}
+
+// CreateContactUsRequest is the struct which describes and represents, new contact us creation fields
+type CreateContactUsRequest struct {
+	Email       string `json:"email" validate:"required,email,max=255"`
+	Title       string `json:"title" validate:"required,min=2,max=255"`
+	Description string `json:"description" validate:"required,min=2,max=500"`
+}
