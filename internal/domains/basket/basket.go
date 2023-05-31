7aa031e6dea2002ae7029025b55d88b26062ea90
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-05-31 17:04:40 +0330

    Add basket module Models and abstraction methods

--- internal/domains/basket/basket.go
+++ internal/domains/basket/basket.go
@@ -1,1 +1,36 @@
 package basket
+
+import "context"
+
+// BasketRepositoryInterface defines set of methods that represent basket's repository functionalities
+type BasketRepositoryInterface interface {
+	GetUserActiveBasket(userId int) (*Basket, error)
+	GetBasketById(id int) (*Basket, error)
+	GetUserBaskets(userId int) ([]Basket, error)
+	GetBasketProduct(basketId int, productId int) (*BasketProducts, error)
+	BasketExists(basketId int) bool
+	CreateBasket(userBasket *Basket) error
+	DisableBasket(userBasket *Basket) error
+	AddProductToBasket(userBasket *Basket, basketProduct *BasketProducts) error
+	UpdateBasketProducts(userBasket *Basket, basketProduct *BasketProducts) error
+}
+
+// BasketServiceInterface defines set of methods that represent basket's service functionalities
+type BasketServiceInterface interface {
+	GetUserActiveBasket(userId int) (*Basket, error)
+	GetUserBaskets(userId int) ([]Basket, error)
+	CreateBasket(userId int) (*Basket, error)
+	DisableActiveBasket() error
+	AddProductsToBasket(productId, amount int) (*Basket, error)        //Todo should fetch product price from Product domain
+	UpdateBasketProductsAmount(productId, amount int) (*Basket, error) //Todo should fetch product price from Product domain
+}
+
+// BasketUseCaseInterface defines set of methods that represent basket's use case functionalities
+type BasketUseCaseInterface interface {
+	GetUserActiveBasket(ctx context.Context, token string) (*Basket, error)
+	GetUserBaskets(ctx context.Context, token string) ([]Basket, error)
+	CreateBasket(ctx context.Context, token string) (*Basket, error)
+	DisableActiveBasket(ctx context.Context, token string) error
+	AddProductsToBasket(ctx context.Context, token string, request *AddProductsToBasketRequest) (*Basket, error)         //Todo should fetch product price from Product domain
+	UpdateBasketProductsAmount(ctx context.Context, token string, request *EditProductsToBasketRequest) (*Basket, error) //Todo should fetch product price from Product domain
+}
--- internal/domains/basket/models.go
+++ internal/domains/basket/models.go
@@ -1,1 +1,36 @@
 package basket
+
+import (
+	"time"
+)
+
+// Basket struct represent basket entity in system
+type Basket struct {
+	Id        int              `json:"id" gorm:"primaryKey"`
+	UserId    int              `json:"-"`
+	Status    bool             `json:"status" gorm:"index"`
+	Products  []BasketProducts `json:"products" gorm:"many2many:basket_products;foreignKey:Id;joinForeignKey:BasketId;References:Id;joinReferences:ProductId"`
+	CreatedAt *time.Time       `json:"created_at"`
+	UpdateAt  *time.Time       `json:"update_at"`
+	DeletedAt *time.Time       `json:"deleted_at"`
+}
+
+// BasketProducts is the struct which represents basket_products table
+type BasketProducts struct {
+	BasketId  int     `json:"basket_id"`
+	ProductId int     `json:"product_id"`
+	Amount    int     `json:"amount"`
+	UnitPrice float64 `json:"unit_price"`
+}
+
+// AddProductsToBasketRequest is the struct which represents add request
+type AddProductsToBasketRequest struct {
+	ProductId int `json:"product_id" validate:"required,min=1"`
+	Amount    int `json:"amount" validate:"required,min=1"`
+}
+
+// EditProductsToBasketRequest is the struct which represents add request
+type EditProductsToBasketRequest struct {
+	ProductId int `json:"product_id" validate:"required,min=1"`
+	Amount    int `json:"amount" validate:"required,min=1"`
+}
