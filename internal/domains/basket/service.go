bef52825ff10483313a5c97c169701e5c9f7df5c
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-06-03 19:56:41 +0330

    Add base methods and tests for basket service

--- internal/domains/basket/service.go
+++ internal/domains/basket/service.go
@@ -1,1 +1,49 @@
 package basket
+
+import (
+	"shop/internal/domains/products"
+)
+
+// BasketService is the type which implements BasketServiceInterface
+type BasketService struct {
+	basketRepo     BasketRepositoryInterface
+	productService products.ProductServiceInterface
+}
+
+// NewBasketService and return it
+func NewBasketService(basketRepo BasketRepositoryInterface, productSv products.ProductServiceInterface) BasketServiceInterface {
+	return &BasketService{
+		basketRepo:     nil,
+		productService: nil,
+	}
+}
+
+func (b *BasketService) GetUserActiveBasket(userId int) (*Basket, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b *BasketService) GetUserBaskets(userId int) ([]Basket, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b *BasketService) CreateBasket(userId int) (*Basket, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b *BasketService) DisableActiveBasket() error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b *BasketService) AddProductsToBasket(productId, amount int) (*Basket, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (b *BasketService) UpdateBasketProductsAmount(productId, amount int) (*Basket, error) {
+	//TODO implement me
+	panic("implement me")
+}
---
+++ internal/domains/basket/service_test.go
@@ -0,0 +1,33 @@
+package basket
+
+import "testing"
+
+// TestBasketService_GetUserActiveBasket functionality
+func TestBasketService_GetUserActiveBasket(t *testing.T) {
+
+}
+
+// TestBasketService_GetUserBaskets functionality
+func TestBasketService_GetUserBaskets(t *testing.T) {
+
+}
+
+// TestBasketService_CreateBasket functionality
+func TestBasketService_CreateBasket(t *testing.T) {
+
+}
+
+// TestBasketService_AddProductsToBasket functionality
+func TestBasketService_AddProductsToBasket(t *testing.T) {
+
+}
+
+// TestBasketService_DisableActiveBasket functionality
+func TestBasketService_DisableActiveBasket(t *testing.T) {
+
+}
+
+// TestBasketService_UpdateBasketProductsAmount functionality
+func TestBasketService_UpdateBasketProductsAmount(t *testing.T) {
+
+}
