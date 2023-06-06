344390e85d8eec30038a6333b2aa9195764b653f
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-06-06 18:09:24 +0330

    Implement Discount's Use Case get all discounts method

--- internal/domains/discount/usecase.go
+++ internal/domains/discount/usecase.go
@@ -1,1 +1,32 @@
 package discount
+
+import "context"
+
+// DiscountUseCase is the type which implements DiscountUseCaseInterface
+type DiscountUseCase struct {
+	sv DiscountServiceInterface
+}
+
+// NewUseCase and return it
+func NewUseCase(sv DiscountServiceInterface) DiscountUseCaseInterface {
+	return &DiscountUseCase{
+		sv: sv,
+	}
+}
+
+// GetAllDiscounts and return the
+func (d *DiscountUseCase) GetAllDiscounts(ctx context.Context, request *GetAllDiscountCodesRequest) ([]DiscountCode, error) {
+	return d.sv.GetAllDiscounts(request.From, request.Limit)
+}
+
+// GetDiscountById and return it
+func (d *DiscountUseCase) GetDiscountById(ctx context.Context, id int) (*DiscountCode, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+// GetDiscountByCode and return it
+func (d DiscountUseCase) GetDiscountByCode(ctx context.Context, code string) (*DiscountCode, error) {
+	//TODO implement me
+	panic("implement me")
+}
---
+++ internal/domains/discount/usecase_test.go
@@ -0,0 +1,52 @@
+package discount
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+	"testing"
+)
+
+// TestDiscountUseCase_GetAllDiscounts functionality
+func TestDiscountUseCase_GetAllDiscounts(t *testing.T) {
+	db, err := setupDbConnection()
+	assert.NoError(t, err, "setting up database connection failed")
+	testingObjCounts := 2
+
+	uc := createDiscountUseCase(db)
+	mockedGetAllRequest := mockGetAllDiscountsRequest()
+	ctx := context.Background()
+
+	mockedDiscounts := mockAndInsertDiscountCodes(db, testingObjCounts)
+	defer destructDiscountCodes(db, mockedDiscounts)
+	assert.Equal(t, len(mockedDiscounts), testingObjCounts, "Initializing discount codes mocked objects failed !Required amounts and created amounts are not equal")
+
+	result, err := uc.GetAllDiscounts(ctx, mockedGetAllRequest)
+	assert.Equal(t, len(result), testingObjCounts, "fetching discount codes failed. fetched items length with mocked items length are not equal")
+	assert.NoError(t, err, "Fetching discounts with id from discount service failed")
+
+	assertDiscountCodes(t, mockedDiscounts, result)
+}
+
+// TestDiscountUseCase_GetDiscountByCode functionality
+func TestDiscountUseCase_GetDiscountByCode(t *testing.T) {
+
+}
+
+// TestDiscountUseCase_GetDiscountById functionality
+func TestDiscountUseCase_GetDiscountById(t *testing.T) {
+
+}
+
+// createDiscountUseCase and return it for testing purpose
+func createDiscountUseCase(db *gorm.DB) DiscountUseCaseInterface {
+	return NewUseCase(NewService(NewRepository(db)))
+}
+
+// mockGetAllDiscountsRequest and return them
+func mockGetAllDiscountsRequest() *GetAllDiscountCodesRequest {
+	return &GetAllDiscountCodesRequest{
+		From:  0,
+		Limit: 10,
+	}
+}
