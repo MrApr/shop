153c3103f99a645c168d51b6060f3ee20e26e668
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-06-06 15:40:41 +0330

    Add Discount models and base functionalities

--- internal/domains/discount/discount.go
+++ internal/domains/discount/discount.go
@@ -1,1 +1,24 @@
 package discount
+
+import "context"
+
+// DiscountRepositoryInterface defines set of abstract methods for discount repo
+type DiscountRepositoryInterface interface {
+	GetAllDiscounts(from, to int) []DiscountCode
+	GetDiscountById(id int) *DiscountCode
+	GetDiscountByCode(code string) *DiscountCode
+}
+
+// DiscountServiceInterface defines set of abstract methods that each type who wants to be known and treated as discount service should obey
+type DiscountServiceInterface interface {
+	GetAllDiscounts(from, to int) ([]DiscountCode, error)
+	GetDiscountById(id int) (*DiscountCode, error)
+	GetDiscountByCode(code string) (*DiscountCode, error)
+}
+
+// DiscountUseCaseInterface defines set of abstract methods that each type who wants to be known and treated as discount use-case should obey
+type DiscountUseCaseInterface interface {
+	GetAllDiscounts(ctx context.Context, request *GetAllDiscountCodesRequest) ([]DiscountCode, error)
+	GetDiscountById(ctx context.Context, id int) (*DiscountCode, error)
+	GetDiscountByCode(ctx context.Context, code string) (*DiscountCode, error)
+}
--- internal/domains/discount/models.go
+++ internal/domains/discount/models.go
@@ -1,1 +1,21 @@
 package discount
+
+import "time"
+
+// DiscountCode is the struct which represents discount entity in system
+type DiscountCode struct {
+	Id              int        `json:"id" gorm:"primaryKey"`
+	Title           string     `json:"title"`
+	Code            string     `json:"code" gorm:"uniqueIndex"`
+	DiscountPercent int        `json:"discount_percent"`
+	Status          bool       `json:"status"`
+	CreatedAt       *time.Time `json:"created_at"`
+	UpdateAt        *time.Time `json:"update_at"`
+	DeletedAt       *time.Time `json:"deleted_at"`
+}
+
+// GetAllDiscountCodesRequest for incoming http requests that want to fetch all discount codes
+type GetAllDiscountCodesRequest struct {
+	From  int `json:"from" validate:"required,min=0"`
+	Limit int `json:"Limit" validate:"required,min=1"`
+}
