f391ab08eda87f11d22cc53f4d84a14b9be235e7
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-06-07 19:11:27 +0330

    Add post type domain's methods and entity

--- internal/domains/postType/models.go
+++ internal/domains/postType/models.go
@@ -1,1 +1,14 @@
 package postType
+
+import "time"
+
+// PostType struct represents post type entity in system
+type PostType struct {
+	Id              int        `json:"id" gorm:"primaryKey"`
+	Title           string     `json:"title"`
+	Price           float64    `json:"price"`
+	DeliverableTime time.Time  `json:"deliverable_time"`
+	CreatedAt       *time.Time `json:"created_at"`
+	UpdateAt        *time.Time `json:"update_at"`
+	DeletedAt       *time.Time `json:"deleted_at"`
+}
--- internal/domains/postType/postType.go
+++ internal/domains/postType/postType.go
@@ -1,1 +1,20 @@
 package postType
+
+import "context"
+
+// PostTypeRepositoryInterface defines set of methods for every type who is going to be and used as Repository for post type
+type PostTypeRepositoryInterface interface {
+	GetAllPostTypes() []PostType
+	PostTypeExists(id int) bool
+}
+
+// PostTypeServiceInterface defines set of methods for every type who is going to be and used as Service for post type
+type PostTypeServiceInterface interface {
+	GetAllPostTypes() ([]PostType, error)
+	PostTypeExists(id int) bool
+}
+
+// PostTypeUseCaseInterface defines set of methods for every type who is going to be and used as Use Case for post type
+type PostTypeUseCaseInterface interface {
+	GetAllPostTypes(ctx context.Context) ([]PostType, error)
+}
