b7feb172a27d52a45e179e50977c97224af65c6f
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-06-09 20:06:47 +0330

    Add gateway use-case alongside it's test and implement its get all functionality

--- internal/domains/gateways/usecases.go
+++ internal/domains/gateways/usecases.go
@@ -7,6 +7,11 @@ type GatewayTypeUseCase struct {
 	sv GatewayTypesServiceInterface
 }
 
+// GatewayUseCase is the struct which implements GatewayUseCaseInterface
+type GatewayUseCase struct {
+	sv GatewayServiceInterface
+}
+
 func NewGatewayTypeUseCase(sv GatewayTypesServiceInterface) GatewayTypesUseCaseInterface {
 	return &GatewayTypeUseCase{
 		sv: sv,
@@ -16,4 +21,18 @@ func NewGatewayTypeUseCase(sv GatewayTypesServiceInterface) GatewayTypesUseCaseI
 // GetAllTypes and return them
 func (g *GatewayTypeUseCase) GetAllTypes(ctx context.Context) ([]GatewayType, error) {
 	return g.sv.GetAllTypes()
+}
+
+// NewGatewayUseCase and return it
+func NewGatewayUseCase(sv GatewayServiceInterface) GatewayUseCaseInterface {
+	return &GatewayUseCase{
+		sv: sv,
+	}
+}
+
+// GetAllGateways and return them
+func (g *GatewayUseCase) GetAllGateways(ctx context.Context, request *GetAllGatewaysRequest) ([]GateWay, error) {
+	onlyActives := *request.OnlyActives
+
+	return g.sv.GetAllGateways(request.TypeId, onlyActives)
 }
--- internal/domains/gateways/usecases_test.go
+++ internal/domains/gateways/usecases_test.go
@@ -27,7 +27,43 @@ func TestGatewayTypeUseCase_GetAllTypes(t *testing.T) {
 	assertGatewayTypes(t, mockedTypes, result)
 }
 
+// TestGatewayUseCase_GetAllGateways functionality
+func TestGatewayUseCase_GetAllGateways(t *testing.T) {
+	db, err := setupDbConnection()
+	assert.NoError(t, err, "Setting up database connection failed")
+
+	uC := createGatewayUseCase(db)
+	testingObjCount := 2
+	ctx := context.Background()
+
+	mockedTypes := mockAndInsertGatewayTypes(db, 1, false)
+	defer destructMockedTypes(db, mockedTypes)
+
+	mockedGetAllRequest := mockGetAllGatewaysRequest(mockedTypes[0].Id, true)
+
+	mockedGateways := mockAndInsertGateways(db, testingObjCount, mockedTypes[0].Id, true)
+	defer destructMockedGateways(db, mockedGateways)
+	assert.Equal(t, len(mockedGateways), testingObjCount, "Mocked objects length is not as equal as required and expected")
+
+	result, err := uC.GetAllGateways(ctx, mockedGetAllRequest)
+	assert.NoError(t, err, "expected no error on fetching all gateways")
+
+	assertGateways(t, mockedGateways, result)
+}
+
 // createGatewayTypeUseCase and return it
 func createGatewayTypeUseCase(db *gorm.DB) GatewayTypesUseCaseInterface {
 	return NewGatewayTypeUseCase(NewGatewayTypesService(NewGatewayTypeRepo(db)))
+}
+
+// createGatewayUseCase and return it
+func createGatewayUseCase(db *gorm.DB) GatewayUseCaseInterface {
+	return NewGatewayUseCase(NewGatewayService(NewGatewayRepository(db)))
+}
+
+func mockGetAllGatewaysRequest(typeId int, onlyActives bool) *GetAllGatewaysRequest {
+	return &GetAllGatewaysRequest{
+		TypeId:      typeId,
+		OnlyActives: &onlyActives,
+	}
 }
