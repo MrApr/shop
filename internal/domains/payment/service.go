a7b6e8808b62ddeaab1c30d10d7c160e11e0b13b
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-06-13 16:11:22 +0330

    Add payment service payment creation

--- internal/domains/payment/service.go
+++ internal/domains/payment/service.go
@@ -1,14 +1,43 @@
 package payment
 
+import (
+	"shop/pkg/advancedError"
+	"shop/pkg/paymentHandler"
+)
+
 // PaymentStorageService struct which implements PaymentStorageServiceContract
 type PaymentStorageService struct {
-	repo PaymentRepoContract
+	repo              PaymentRepoContract
+	basketValidator   func(int) error
+	addressValidator  func(int) error
+	discountValidator func(int) error
+	postTypeValidator func(int) error
 }
 
 // NewPaymentStorageService for storage common issues
-func NewPaymentStorageService(repo PaymentRepoContract) PaymentStorageServiceContract {
+func NewPaymentStorageService(repo PaymentRepoContract, basketValidator, addressValidator, discountValidator, postTypeValidator func(id int) error) PaymentStorageServiceContract {
+	if basketValidator == nil {
+		basketValidator = paymentHandler.BasketIsValid
+	}
+
+	if addressValidator == nil {
+		addressValidator = paymentHandler.AddressIsValid
+	}
+
+	if discountValidator == nil {
+		discountValidator = paymentHandler.IsDiscountValid
+	}
+
+	if postTypeValidator == nil {
+		postTypeValidator = paymentHandler.IsPostTypeValid
+	}
+
 	return &PaymentStorageService{
-		repo: repo,
+		repo:              repo,
+		basketValidator:   basketValidator,
+		addressValidator:  addressValidator,
+		discountValidator: discountValidator,
+		postTypeValidator: postTypeValidator,
 	}
 }
 
@@ -23,6 +52,46 @@ func (p *PaymentStorageService) GetPayment(id int) (*Payment, error) {
 
 // CreatePayment and return it based on given Data
 func (p *PaymentStorageService) CreatePayment(userId, BasketId, addressId, discountId, gatewayId, postTypeId int, totalPrice float64) (*Payment, error) {
-	//TODO implement me
-	panic("implement me")
+	payment := Payment{
+		UserId:     userId,
+		BasketId:   BasketId,
+		AddressId:  addressId,
+		DiscountId: discountId,
+		GatewayId:  gatewayId,
+		PostTypeId: postTypeId,
+		TotalPrice: totalPrice,
+		Status:     PaymentDefaultStatus,
+	}
+
+	err := p.paymentCanCreated(&payment)
+	if err != nil {
+		return nil, err
+	}
+
+	err = p.repo.CreatePayment(&payment)
+	if err != nil {
+		return nil, advancedError.New(err, "cannot create a new payment")
+	}
+	return &payment, nil
+}
+
+// paymentCanCreated checks whether payment is allowed to insert in db or not!
+func (p *PaymentStorageService) paymentCanCreated(payment *Payment) error {
+	if err := p.basketValidator(payment.BasketId); err != nil {
+		return err
+	}
+
+	if err := p.addressValidator(payment.AddressId); err != nil {
+		return err
+	}
+
+	if err := p.discountValidator(payment.DiscountId); err != nil {
+		return err
+	}
+
+	if err := p.postTypeValidator(payment.PostTypeId); err != nil {
+		return err
+	}
+
+	return nil
 }
--- internal/domains/payment/service_test.go
+++ internal/domains/payment/service_test.go
@@ -1,6 +1,7 @@
 package payment
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"gorm.io/gorm"
 	"math/rand"
@@ -12,7 +13,7 @@ func TestPaymentStorageService_GetPayment(t *testing.T) {
 	conn, err := setupDbConnection()
 	assert.NoError(t, err, "Cannot make connection to database")
 
-	sv := createPaymentService(conn)
+	sv := createPaymentService(conn, false)
 	payments := mockAndInsertData(conn, 1, false)
 	defer destructPayments(conn, payments)
 	assert.Equal(t, 1, len(payments), "Mocking payments failed")
@@ -32,28 +33,43 @@ func TestPaymentStorageService_GetPayment(t *testing.T) {
 
 // TestPaymentService_CreatePayment functionality
 func TestPaymentService_CreatePayment(t *testing.T) {
-	//conn, err := setupDbConnection()
-	//assert.NoError(t, err, "Cannot make connection to database")
-	//
-	//sv := createPaymentService(conn)
-	//mockedPaymentable := mockPaymentable()
-	//userId := rand.Int()
+	conn, err := setupDbConnection()
+	assert.NoError(t, err, "Cannot make connection to database")
 
-	//result, err := sv.CreatePayment(&mockedPaymentable, userId)
-	//defer destructPayments(conn, []Payment{*result})
-	//assert.NoError(t, err, "Payment service payment creation error")
-	//
-	//assert.Equal(t, result.UserId, userId, "Payment service payment creation error")
-	//assert.Equal(t, result.Amount, mockedPaymentable.Amount(), "Payment service payment creation error")
-	//assert.Equal(t, result.PaymentableID, mockedPaymentable.PaymentableId(), "Payment service payment creation error")
-	//assert.Equal(t, result.PaymentableType, mockedPaymentable.PaymentableType(), "Payment service payment creation error")
-	//assert.NotNil(t, result.CreatedAt, "Payment service payment creation error")
-	//assert.NotNil(t, result.UpdatedAt, "Payment service payment creation error")
+	sv := createPaymentService(conn, false)
+	userId := rand.Int()
+	basketId := rand.Int()
+	addressId := rand.Int()
+	discountId := rand.Int()
+	gatewayId := rand.Int()
+	postTypeId := rand.Int()
+	totalPrice := rand.Float64()
+
+	_, err = sv.CreatePayment(userId, basketId, addressId, discountId, gatewayId, postTypeId, totalPrice)
+	assert.Error(t, err, "Expected error for validation function rejection")
+
+	sv = createPaymentService(conn, true)
+	result, err := sv.CreatePayment(userId, basketId, addressId, discountId, gatewayId, postTypeId, totalPrice)
+	defer destructPayments(conn, []Payment{*result})
+	assert.NoError(t, err, "Payment service payment creation error")
+
+	assert.Equal(t, result.UserId, userId, "Payment service payment creation error")
+	assert.Equal(t, result.BasketId, basketId, "Payment service payment creation error")
+	assert.Equal(t, result.AddressId, addressId, "Payment service payment creation error")
+	assert.Equal(t, result.DiscountId, discountId, "Payment service payment creation error")
+	assert.Equal(t, result.GatewayId, gatewayId, "Payment service payment creation error")
+	assert.Equal(t, result.PostTypeId, postTypeId, "Payment service payment creation error")
+	assert.Equal(t, result.TotalPrice, totalPrice, "Payment service payment creation error")
+	assert.NotNil(t, result.CreatedAt, "Payment service payment creation error")
+	assert.NotNil(t, result.UpdatedAt, "Payment service payment creation error")
 }
 
 // createPaymentService and return it
-func createPaymentService(db *gorm.DB) PaymentStorageServiceContract {
-	return NewPaymentStorageService(NewPaymentRepo(db))
+func createPaymentService(db *gorm.DB, withValidFunc bool) PaymentStorageServiceContract {
+
+	validFunc := generateValidationFunction(withValidFunc)
+
+	return NewPaymentStorageService(NewPaymentRepo(db), validFunc, validFunc, validFunc, validFunc)
 }
 
 // customPaymentableType defines a struct which implements paymentable interface
@@ -84,5 +100,18 @@ func mockPaymentable() customPaymentableType {
 		paymentableType: "TestType",
 		paymentableId:   rand.Int(),
 		amount:          rand.Float64(),
+	}
+}
+
+// generateValidationFunction for payment service when a new payment is going to get created
+func generateValidationFunction(withValid bool) func(int) error {
+	if withValid {
+		return func(int) error {
+			return nil
+		}
+	}
+
+	return func(int) error {
+		return errors.New("test error validation function")
 	}
 }
