d8d919b12d5599f3de54b3774654338f744dcdde
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-06-13 14:23:05 +0330

    Improvement on payment migration Add payment module base methods, models and entities

--- internal/domains/payment/models.go
+++ internal/domains/payment/models.go
@@ -1,1 +1,44 @@
 package payment
+
+import "time"
+
+// Payment defines set of fields for payment entity in system
+type Payment struct {
+	Id         int        `json:"id" gorm:"primaryKey"`
+	UserId     int        `json:"user_id"`
+	BasketId   int        `json:"basket_id"`
+	AddressId  int        `json:"address_id"`
+	DiscountId int        `json:"discount_id"`
+	GatewayId  int        `json:"gateway_id"`
+	PostTypeId int        `json:"post_type_id"`
+	TotalPrice float64    `json:"total_price"`
+	RefNum     string     `json:"ref_num" gorm:"uniqueIndex"`
+	TraceNum   string     `json:"trace_num" gorm:"uniqueIndex"`
+	Status     string     `json:"status"`
+	CreatedAt  *time.Time `json:"created_at"`
+	UpdateAt   *time.Time `json:"update_at"`
+	DeletedAt  *time.Time `json:"deleted_at"`
+}
+
+// CreatePaymentRequest defines set of methods for payment creation
+type CreatePaymentRequest struct {
+	BasketId   int     `json:"basket_id" validate:"required,min=1"`
+	AddressId  int     `json:"address_id" validate:"required,min=1"`
+	DiscountId int     `json:"discount_id" validate:"required,min=1"`
+	GatewayId  int     `json:"gateway_id" validate:"required,min=1"`
+	PostTypeId int     `json:"post_type_id" validate:"required,min=1"`
+	TotalPrice float64 `json:"total_price" validate:"required,min=1"`
+}
+
+// PaymentVerifyRequest defines set of fields that are required for payment verification
+type PaymentVerifyRequest struct {
+	PaymentId int    `query:"payment_id" validate:"required,min=1"`
+	Authority string `query:"authority" validate:"required,min=36,max=36"`
+}
+
+// RequestPaymentResponse defines set of fields in which is returned by payment gateway before starting payment operation and redirecting to bank
+type RequestPaymentResponse struct {
+	Url             string
+	Key             string
+	OperationStatus int
+}
--- internal/domains/payment/payment.go
+++ internal/domains/payment/payment.go
@@ -1,1 +1,31 @@
 package payment
+
+import "context"
+
+// PaymentRepoContract defines set of methods that every type, which wants to play payment repository role, should obey
+type PaymentRepoContract interface {
+	GetPayment(id int) (*Payment, error)
+	GetUserLastPayment(userId int, pendPayment bool) (*Payment, error)
+	CreatePayment(payment *Payment) error
+	UpdatePaymentRef(payment *Payment, refNum string) (*Payment, error)
+	UpdatePaymentTraceStatus(payment *Payment, traceNum, status string) (*Payment, error)
+}
+
+// PaymentStorageServiceContract defines set of methods that every type, which wants to play payment service role, should obey
+type PaymentStorageServiceContract interface {
+	GetPayment(id int) (*Payment, error)
+	CreatePayment(userId, BasketId, addressId, discountId, gatewayId, postTypeId int, totalPrice float64) (*Payment, error)
+}
+
+// PaymentPGWServiceContract defines set of methods that every implementation of payment gateway should obey
+type PaymentPGWServiceContract interface {
+	Pay(paymentId int) (*RequestPaymentResponse, error)
+	Verify(paymentId int, Authority string) (*Payment, error)
+}
+
+// PaymentUseCaseContract defines set of methods that every type, which wants to play payment use case role, should obey
+type PaymentUseCaseContract interface {
+	CreatePayment(ctx context.Context, token string, request *CreatePaymentRequest) (*Payment, error)
+	Pay(ctx context.Context, token string, paymentId int) (*RequestPaymentResponse, error)
+	Verify(ctx context.Context, token string, request *PaymentVerifyRequest) (*Payment, error)
+}
