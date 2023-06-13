e378cd68887876beb2351390afa01372f82b07b5
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-06-13 16:49:44 +0330

    Add usecase

--- internal/domains/payment/errors.go
+++ internal/domains/payment/errors.go
@@ -3,5 +3,7 @@ package payment
 import "errors"
 
 var (
-	PaymentNotFound error = errors.New("no payment found with given credentials")
+	PaymentNotFound    error = errors.New("no payment found with given credentials")
+	Unauthorized       error = errors.New("user is not allowed to do the operation")
+	SomethingWentWrong error = errors.New("something went wrong\n Please contact administrator")
 )
--- internal/domains/payment/usecase.go
+++ internal/domains/payment/usecase.go
@@ -1,1 +1,92 @@
 package payment
+
+import (
+	"context"
+	"gorm.io/gorm"
+	"shop/pkg/advancedError"
+)
+
+// PaymentUseCase is the usecase which implements payment use case contract and satisfies all of it's methods
+type PaymentUseCase struct {
+	paymentSvPGW     PaymentPGWServiceContract
+	paymentSvStorage PaymentStorageServiceContract
+	tokenDecoder     func(ctx context.Context, token string) (int, error)
+	db               *gorm.DB
+}
+
+// NewPaymentUseCase creates and returns payment usecase
+func NewPaymentUseCase(svPgw PaymentPGWServiceContract, svStorage PaymentStorageServiceContract, db *gorm.DB, decoder func(ctx context.Context, token string) (int, error)) PaymentUseCaseContract {
+	return &PaymentUseCase{
+		paymentSvPGW:     svPgw,
+		paymentSvStorage: svStorage,
+		db:               db,
+		tokenDecoder:     decoder,
+	}
+}
+
+// CreatePayment and return it
+func (p *PaymentUseCase) CreatePayment(ctx context.Context, token string, request *CreatePaymentRequest) (*Payment, error) {
+	userId, err := p.tokenDecoder(ctx, token)
+	if err != nil {
+		return nil, err
+	}
+
+	return p.paymentSvStorage.CreatePayment(userId, request.BasketId, request.AddressId, request.DiscountId, request.GatewayId, request.PostTypeId, request.TotalPrice)
+}
+
+// Pay operates payment
+func (p *PaymentUseCase) Pay(ctx context.Context, token string, paymentId int) (*RequestPaymentResponse, error) {
+	doesntHavePermission := p.userCanPay(ctx, paymentId, token)
+	if doesntHavePermission != nil {
+		return nil, doesntHavePermission
+	}
+
+	return p.paymentSvPGW.Pay(paymentId)
+}
+
+// Verify done payment by user
+func (p *PaymentUseCase) Verify(ctx context.Context, token string, request *PaymentVerifyRequest) (*Payment, error) {
+	doesntHavePermission := p.userCanPay(ctx, request.PaymentId, token)
+	if doesntHavePermission != nil {
+		return nil, doesntHavePermission
+	}
+
+	tx := p.db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	result, err := p.paymentSvPGW.Verify(request.PaymentId, request.Authority)
+	if err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	err = tx.Commit().Error
+	if err != nil {
+		return nil, advancedError.New(err, SomethingWentWrong.Error())
+	}
+
+	return result, nil
+}
+
+// userCanPay check whether user can do required operation or not
+func (p *PaymentUseCase) userCanPay(ctx context.Context, paymentId int, token string) error {
+	userId, err := p.tokenDecoder(ctx, token)
+	if err != nil {
+		return err
+	}
+
+	payment, err := p.paymentSvStorage.GetPayment(paymentId)
+	if err != nil {
+		return err
+	}
+
+	if payment.UserId != userId {
+		return Unauthorized
+	}
+
+	return nil
+}
