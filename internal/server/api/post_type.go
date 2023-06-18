e4039a78b527637671b21cd3164e5adad93468f7
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-06-18 17:09:23 +0330

    Add post types handler in order to get attached with post types domain

--- internal/server/api/post_type.go
+++ internal/server/api/post_type.go
@@ -1,1 +1,41 @@
 package api
+
+import (
+	"context"
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+	"net/http"
+	"shop/internal/domains/postType"
+)
+
+// postTypeEchoHandler is the type which attaches rest api end points to domain functions
+type postTypeEchoHandler struct {
+	uC postType.PostTypeUseCaseInterface
+}
+
+// AttachPostTypeHandlerToPostTypeDomain for working with rest Apis
+func AttachPostTypeHandlerToPostTypeDomain(engine *echo.Echo, db *gorm.DB) {
+	uC := postType.NewUseCase(postType.NewPostTypeService(postType.NewRepository(db)))
+
+	setupPostTypeHandler(engine, &postTypeEchoHandler{
+		uC: uC,
+	})
+}
+
+// setupPostTypeHandler which are accessible through http URI
+func setupPostTypeHandler(engine *echo.Echo, handler *postTypeEchoHandler) {
+	postTypeRouter := engine.Group("/post_types")
+	postTypeRouter.GET("", handler.GetAllPostTypes)
+}
+
+// GetAllPostTypes and return them
+func (pH *postTypeEchoHandler) GetAllPostTypes(e echo.Context) error {
+	ctx := context.Background()
+
+	postTypes, err := pH.uC.GetAllPostTypes(ctx)
+	if err != nil {
+		return e.JSON(http.StatusNotFound, generateResponse(nil, err))
+	}
+
+	return e.JSON(http.StatusOK, generateResponse(postTypes, nil))
+}
