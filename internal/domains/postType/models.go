0b59f98cce3ed8a7df1e8b8dd44629027d8ae10d
Mohammad Reza Ahmadi Pour <mamali0938@gmail.com>
2023-08-09 12:29:19 +0330

    Fixes on post types modules

--- internal/domains/postType/models.go
+++ internal/domains/postType/models.go
@@ -1,14 +1,17 @@
 package postType
 
-import "time"
+import (
+	"gorm.io/gorm"
+	"time"
+)
 
 // PostType struct represents post type entity in system
 type PostType struct {
-	Id              int        `json:"id" gorm:"primaryKey"`
-	Title           string     `json:"title"`
-	Price           float64    `json:"price"`
-	DeliverableTime time.Time  `json:"deliverable_time"`
-	CreatedAt       *time.Time `json:"created_at,omitempty"`
-	UpdatedAt       *time.Time `json:"updated_at,omitempty"`
-	DeletedAt       *time.Time `json:"deleted_at,omitempty"`
+	Id              int            `json:"id" gorm:"primaryKey"`
+	Title           string         `json:"title"`
+	Price           float64        `json:"price"`
+	DeliverableTime uint64         `json:"deliverable_time"` //In minutes
+	CreatedAt       *time.Time     `json:"created_at,omitempty"`
+	UpdatedAt       *time.Time     `json:"updated_at,omitempty"`
+	DeletedAt       gorm.DeletedAt `json:"deleted_at,omitempty"`
 }
